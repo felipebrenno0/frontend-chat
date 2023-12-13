03b37d39e6f754ec5b8d3799cb7212cd33818355
felipebrenno0 <felipebrenno213@gmail.com>
2023-12-13 04:11:33 -0300

    change server url

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://backend-chat-49zv.onrender.com")
     //websocket.onopen = () => websocket.send(`${user.name} entrou`)
     //websocket.onclose = () => websocket.send(`${user.name} saiu`)
 
